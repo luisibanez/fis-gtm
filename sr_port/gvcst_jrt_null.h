dee71008ddc86a6d5b91e6fdb0dc61e82bee6a62
Luis Ibanez <luis.ibanez@kitware.com>
2012-03-24 14:06:46 -0400

    ENH: Version 5.5000 from sourceforge.
    Source code taken from here:
    http://sourceforge.net/projects/fis-gtm/files/GT.M-x86-Linux-src/V5.5-000/

---
+++ sr_port/gvcst_jrt_null.h
@@ -0,0 +1,18 @@
+/****************************************************************
+ *								*
+ *	Copyright 2012 Fidelity Information Services, Inc	*
+ *								*
+ *	This source code contains the intellectual property	*
+ *	of its copyright holder(s), and is made available	*
+ *	under a license.  If you do not know the terms of	*
+ *	the license, please stop and do not read further.	*
+ *								*
+ ****************************************************************/
+
+#ifndef __GVCST_JRT_NULL_H__
+#define __GVCST_JRT_NULL_H__
+
+void	gvcst_jrt_null(void);
+
+#endif
+
