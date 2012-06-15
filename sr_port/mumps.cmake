8371effcd1d6399a4827e958ed6af6aa3015aaa3
Brad King <brad.king@kitware.com>
2012-06-15 16:26:42 -0400

    Install GDE*.o instead of GDE*.m

---
+++ sr_port/mumps.cmake
@@ -0,0 +1,15 @@
+#################################################################
+#								#
+#	Copyright 2012 Fidelity Information Services, Inc	#
+#								#
+#	This source code contains the intellectual property	#
+#	of its copyright holder(s), and is made available	#
+#	under a license.  If you do not know the terms of	#
+#	the license, please stop and do not read further.	#
+#								#
+#################################################################
+set(ENV{gtm_dist} "${gtm_dist}")
+set(ENV{gtmroutines} ".")
+execute_process(
+  COMMAND ${mumps} ${input}
+  )
