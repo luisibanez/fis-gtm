a6ecf365b802ec67d9bb3bba8721956b1b842447
Amul Shah <amul.shah@fisglobal.com>
2012-06-15 14:45:20 -0400

    mumps depends on libgtmshr
    ttt and msg depend on their cmake files
    
    add gtmroutines to ttt and msg cmake files
    
    make libgtmshr and libgtmcrypt MODULES from SHARED

--- sr_port/msg.cmake
+++ sr_port/msg.cmake
@@ -1,4 +1,5 @@
 set(ENV{gtm_dist} "${gtm_dist}")
+set(ENV{gtmroutines} ".")
 execute_process(
   COMMAND ${mumps} -run msg ${input} unix
   )
--- sr_port/tttgen.cmake
+++ sr_port/tttgen.cmake
@@ -1,4 +1,5 @@
 set(ENV{gtm_dist} "${gtm_dist}")
+set(ENV{gtmroutines} ".")
 execute_process(
   COMMAND ${mumps} -run tttgen ${input} ${sr_port}/opcode_def.h ${sr_port}/vxi.h
   OUTPUT_FILE ttt.log
