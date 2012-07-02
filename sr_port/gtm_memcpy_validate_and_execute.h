ab79a1f44b5fc40ce58b8d6a3809ea67a0bf6631
Amul Shah <amul.shah@fisglobal.com>
2012-07-02 09:47:25 -0400

    ENH: missed two files

---
+++ sr_port/gtm_memcpy_validate_and_execute.h
@@ -0,0 +1,17 @@
+/****************************************************************
+ *								*
+ *	Copyright 2012 Fidelity Information Services, Inc	*
+ *								*
+ *	This source code contains the intellectual property	*
+ *	of its copyright holder(s), and is made available	*
+ *	under a license.  If you do not know the terms of	*
+ *	the license, please stop and do not read further.	*
+ *								*
+ ****************************************************************/
+
+#ifndef GTM_MVAE_INCLUDED
+#define GTM_MVAE_INCLUDED
+
+void *gtm_memcpy_validate_and_execute(void *target, const void *src, size_t len);
+
+#endif
