c542303eda934ae5725ec6698aa6ca73d8090b05
Luis Ibanez <luis.ibanez@kitware.com>
2012-12-24 16:58:16 -0500

    Got ARM equivalent of the EIP register.
    The Extended Instruction Pointer EIP, seems to be the equivalent
    of the Program Counter PC register in the ARM architecture:
    
    http://infocenter.arm.com/help/index.jsp?topic=/com.arm.doc.dai0274b/index.html
    
    The list of ARM registers can be seen in the structure sigcontext
    in the file:  /usr/include/arm-linux-gnueabihf/asm/sigcontext.h

--- sr_unix/extract_signal_info.c
+++ sr_unix/extract_signal_info.c
@@ -115,6 +115,11 @@ void extract_signal_info(int sig, siginfo_t *info, gtm_sigcontext_t *context, gt
 				{
 #    ifdef __ia64
 					gtmsi->int_iadr = (caddr_t)context->uc_mcontext.sc_ip;
+#    elif defined(__arm__)
+#      ifndef REG_EIP
+#        define REG_EIP EIP
+#      endif
+					gtmsi->int_iadr = (caddr_t)context->uc_mcontext.arm_pc;
 #    elif defined(__i386)
 #      ifndef REG_EIP
 #        define REG_EIP EIP
