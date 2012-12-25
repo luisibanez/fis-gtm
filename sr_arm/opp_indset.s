ef6a3005da8054d3043fe0e7aa3fac64ba19914b
Luis Ibanez <luis.ibanez@kitware.com>
2012-12-25 16:37:09 -0500

    Adding more ARM assembly stubs.

---
+++ sr_arm/opp_indset.s
@@ -0,0 +1,30 @@
+#################################################################
+#								#
+#	Copyright 2001 Sanchez Computer Associates, Inc.	#
+#								#
+#	This source code contains the intellectual property	#
+#	of its copyright holder(s), and is made available	#
+#	under a license.  If you do not know the terms of	#
+#	the license, please stop and do not read further.	#
+#								#
+#################################################################
+
+	.arch armv6
+	.fpu vfp
+	.text
+
+	.align	2
+	.global	opp_indset
+	.type	opp_indset, %function
+opp_indset:
+	@ args = 0, pretend = 0, frame = 0
+	@ frame_needed = 1, uses_anonymous_args = 0
+	@ link register save eliminated.
+	str	fp, [sp, #-4]!
+	add	fp, sp, #0
+	mov	r3, #0
+	mov	r0, r3
+	add	sp, fp, #0
+	ldmfd	sp!, {fp}
+	bx	lr
+	.size	opp_indset, .-opp_indset
